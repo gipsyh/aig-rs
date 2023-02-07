eb3f531be6cce89555600ae1802d3aee90c79a3c
Yuheng Su <gipsyh.icu@gmail.com>
2023-02-07 11:36:08 +0000

    fix bug

--- src/others.rs
+++ src/others.rs
@@ -2,6 +2,6 @@ use crate::{Aig, AigCube, AigEdge};
 
 impl Aig {
     pub fn latch_init_cube(&self) -> AigCube {
-        AigCube::from_iter(self.latchs.iter().map(|l| Into::<AigEdge>::into(l.input)))
+        AigCube::from_iter(self.latchs.iter().map(|l| AigEdge::new(l.input, !l.init)))
     }
 }
