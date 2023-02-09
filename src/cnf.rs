c7fac8e0adea0f79cbd6a359c53440946a742272
Yuheng Su <gipsyh.icu@gmail.com>
2023-02-09 11:37:44 +0000

    update cnf interface

--- src/cnf.rs
+++ src/cnf.rs
@@ -3,7 +3,7 @@ use logic_form::{Clause, Cnf, Lit};
 use std::collections::HashSet;
 
 impl Aig {
-    pub fn get_cnf(&self, logic: &[AigEdge]) -> Cnf {
+    pub fn get_optimized_cnf(&self, logic: &[AigEdge]) -> Cnf {
         let mut refs = HashSet::new();
         for l in logic {
             assert!(*l != AigEdge::constant_edge(true) && *l != AigEdge::constant_edge(false));
@@ -50,6 +50,40 @@ impl Aig {
         }
         for l in logic {
             ans.push(Clause::from([Lit::new(l.node_id().into(), l.compl())]));
+        }
+        ans
+    }
+
+    pub fn get_cnf(&self) -> Cnf {
+        let mut ans = Cnf::new();
+        for i in self.nodes_range() {
+            if self.nodes[i].is_and() {
+                ans.push(Clause::from([
+                    Lit::new(self.nodes[i].node_id().into(), true),
+                    Lit::new(
+                        self.nodes[i].fanin0().node_id().into(),
+                        self.nodes[i].fanin0().compl(),
+                    ),
+                ]));
+                ans.push(Clause::from([
+                    Lit::new(self.nodes[i].node_id().into(), true),
+                    Lit::new(
+                        self.nodes[i].fanin1().node_id().into(),
+                        self.nodes[i].fanin1().compl(),
+                    ),
+                ]));
+                ans.push(Clause::from([
+                    Lit::new(self.nodes[i].node_id().into(), false),
+                    Lit::new(
+                        self.nodes[i].fanin0().node_id().into(),
+                        !self.nodes[i].fanin0().compl(),
+                    ),
+                    Lit::new(
+                        self.nodes[i].fanin1().node_id().into(),
+                        !self.nodes[i].fanin1().compl(),
+                    ),
+                ]));
+            }
         }
         ans
     }
